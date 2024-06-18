1eebd4235cb637a7704e8ebabf7a2e1d40780f3a
ParvathySree <sreeparvathylaiju@gmail.com>
2024-06-18 12:01:35 +0530

    api url changed

--- ScribbleIt/src/assets/constants/constants.jsx
+++ ScribbleIt/src/assets/constants/constants.jsx
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:5000/notes"
+export const API_URL = "https://notes-app-git-main-parvathysrees-projects.vercel.app/notes"
 
 export const colors = [
   "#FAC772", // Yellow
